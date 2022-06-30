50f02b829fdb2f32b6a85998df79550703ff9a59
unknown <julioantony2@gmail.com>
2022-06-30 13:49:49 +0700

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Data from "./questions.json";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">Nutanix NSCE-Core Dump Questions</h1>
+      {Data.map((question, i) => (
+        <div key={i}>
+          <div className="question">
+            <h3 style={{marginRight: "15px"}}>{i + 1}. </h3>
+            <h3>{question.text}</h3>
+          </div>
+          {question.options.map((option, i) => (
+            <div className="option" key={i}>
+              <p>{option}</p>
+            </div>
+          ))}
+          <h5 className="answer">{question.answers}</h5>
+        </div>
+      ))}
     </div>
   );
 }
